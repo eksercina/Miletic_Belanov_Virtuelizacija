ca5829740083a8c1a287b04cbc062335706dbbee
eksercina <mihajlomiletic.vs@gmail.com>
2026-05-12 22:29:35 +0200

    Klase Measurement i ChargingSample

---
+++ Punjac/Common/Contracts/ChargingSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ChargingSample
+    {
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+
+        [DataMember]
+        public Measurement Voltage { get; set; } = new Measurement();
+
+        [DataMember]
+        public Measurement Current { get; set; } = new Measurement();
+
+        [DataMember]
+        public Measurement RealPower { get; set; } = new Measurement();
+
+        [DataMember]
+        public Measurement ReactivePower { get; set; } = new Measurement();
+
+        [DataMember]
+        public Measurement ApparentPower { get; set; } = new Measurement();
+
+        [DataMember]
+        public Measurement Frequency { get; set; } = new Measurement();
+
+        [DataMember]
+        public int RowIndex { get; set; }
+
+        [DataMember]
+        public string VehicleId { get; set; }
+
+        public ChargingSample() { }
+
+        // Helper konstruktor za parsiranje CSVa
+        public ChargingSample(DateTime timestamp, int rowIndex, string vehicleId)
+        {
+            Timestamp = timestamp;
+            RowIndex = rowIndex;
+            VehicleId = vehicleId;
+        }
+    }
+}
---
+++ Punjac/Common/Contracts/Measurement.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class Measurement
+    {
+        [DataMember]
+        public double Min { get; set; }
+
+        [DataMember]
+        public double Avg { get; set; }
+
+        [DataMember]
+        public double Max { get; set; }
+
+        public Measurement() { }
+
+        public Measurement(double min, double avg, double max)
+        {
+            Min = min;
+            Avg = avg;
+            Max = max;
+        }
+
+        public double Range => Max - Min;
+
+        public override string ToString()
+        {
+            return $"Min: {Min:F3}, Avg: {Avg:F3}, Max: {Max:F3}";
+        }
+    }
+}
