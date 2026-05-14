b4e5e006a58a610a5a8559047988a9344ca3ee43
belanovpedja <belanovpedja@gmail.com>
2026-05-14 14:38:01 +0200

    ValidationFault+ IChargingService

---
+++ Punjac/Common/IChargingService.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+using Common;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IChargingService
+    {
+        [OperationContract]
+        [FaultContract(typeof(ValidationFault))]
+        string StartSession(string vehicleId);
+
+        [OperationContract]
+        [FaultContract(typeof(ValidationFault))]
+        void PushSample(ChargingSample sample);
+
+        [OperationContract]
+        void EndSession(string sessionId);
+        void SimulateConnectionBreak();
+    }
+}
---
+++ Punjac/Common/ValidationFaultcs.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ValidationFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Details { get; set; }
+
+        public ValidationFault(string message, string details = null)
+        {
+            Message = message;
+            Details = details ?? string.Empty;
+        }
+    }
+}
