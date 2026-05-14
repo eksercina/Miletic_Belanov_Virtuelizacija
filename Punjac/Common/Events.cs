fe7cd0ae4c814d973aa7d053d437b5035aef3d0b
eksercina <mihajlomiletic.vs@gmail.com>
2026-05-14 17:01:53 +0200

    Events

---
+++ Punjac/Common/Events.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Common
+{
+    public class SampleReceivedEventArgs : EventArgs
+    {
+        public ChargingSample Sample { get; }
+        public string SessionId { get; }
+        public DateTime Timestamp { get; }
+
+        public SampleReceivedEventArgs(ChargingSample sample, string sessionId)
+        {
+            Sample = sample;
+            SessionId = sessionId;
+            Timestamp = DateTime.Now;
+        }
+    }
+
+    public class WarningEventArgs : EventArgs
+    {
+        public string Message { get; }
+        public string VehicleId { get; }
+        public int RowIndex { get; }
+        public string Type { get; }  // VoltageSpike, CurrentSpike, PowerFactorWarning
+
+        public WarningEventArgs(string message, string vehicleId, int rowIndex, string type)
+        {
+            Message = message;
+            VehicleId = vehicleId;
+            RowIndex = rowIndex;
+            Type = type;
+        }
+    }
+}
