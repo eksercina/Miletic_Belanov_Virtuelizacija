10014e7a2611d08c49e790a50fa2c8573a5812ad
belanovpedja <belanovpedja@gmail.com>
2026-05-14 20:27:37 +0200

    Server turning on

--- Punjac/Service/Program.cs
+++ Punjac/Service/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using Service.Services;
 
 namespace Service
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost host = null;
+
+            try
+            {
+                host = new ServiceHost(typeof(ChargingService));
+                host.Open();
+
+                Console.WriteLine("==================================================");
+                Console.WriteLine("     WCF Charging Service - SERVER IS RUNNING");
+                Console.WriteLine("     Address: net.tcp://localhost:4000/ChargingService");
+                Console.WriteLine("==================================================");
+                Console.WriteLine("Press ENTER to shut down the service...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                host?.Close();
+            }
         }
     }
 }
